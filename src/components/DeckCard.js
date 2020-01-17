698983dde6fb5054f557141fd81c61c1cf0667bf
Martin Spicuzza <spicuzza157@gmail.com>
2020-01-17 16:12:11 -0600

    created the deck-viewer grid and components, need toget them set up with actual API data, but the layout is there for further styling

--- src/components/DeckCard.js
+++ src/components/DeckCard.js
@@ -1,24 +1,45 @@
-import React from 'react'
+import React from "react";
 
-const DeckCard = (props) =>{
-    return <div className="deck-card">
-        <div className="container">
-            <p className="row text-center">{props.deck.name !== null ? <p>{props.deck.name}</p>: <p>Deck Name</p> }</p>
-            <div className="row deck-divider ">
-                <div classame="col left"></div>
-                <img classame="col" src="#" alt="set-icon"></div>
-                <div classame="col right"></div>
-            </div>
-            <div className="row">
-                
-            </div>
+const DeckCard = props => {
+  const handleHover = () => {
+    console.log("Hovering");
+  };
+
+  return (
+    <div className="deck-card bg-primary grid-element" onHover={handleHover()}>
+      <div className="container text-center px-2 py-2">
+        <h2 className="col text-center">
+          {props.deck.name !== null ? props.deck.name : "Deck Name"}
+        </h2>
+
+        <div className="row px-3">
+          <img className="col" src="#" alt="page-divider"/>
+          <img
+            className="col bg-danger set-icon"
+            src={props.deck.setIcon !== null ? props.deck.setIcon : "#"}
+            alt="set-icon"
+          />
+          <img className="col" src="#" alt="page-divider"/>
         </div>
-        <div className="upvotes container">
-            <img src="#" alt="upvote-icon"/>
-            {props.deck.upvotes !== null : props.deck.upvotes : <p>X</p> }
+
+        <div className="row">
+          <span className="col">
+            <img src="#" alt="dust-icon" />{" "}
+            {props.deck.dustCost !== null ? props.deck.dustCost : 0}
+          </span>
+          <div className="col" />
+          <div className="col">Curve</div>
         </div>
-        Deck Card
+      </div>
+
+      <div className="upvotes container text-center bg-warning">
+        <p>
+          <img src="#" alt="upvote-icon" />{" "}
+          {props.deck.upvotes !== null ? props.deck.upvotes : "X"}
+        </p>
+      </div>
     </div>
-}
+  );
+};
 
-export default DeckCard
+export default DeckCard;
--- src/containers/DeckCardContainer.js
+++ src/containers/DeckCardContainer.js
@@ -2,13 +2,16 @@ import React from 'react'
 import DeckCard from '../components/DeckCard'
 
 const DeckCardContainer = (props) =>{
+    console.log(props)
     const renderDecks = () =>{
         if(props.decks.length <= 9){
-            props.decks.map(deck => <DeckCard deck={deck}/>)
+            return props.decks.map(deck => <DeckCard deck={deck} key={deck.name}/>)
         }
         else return <p>More than 9</p>
     }
-    return <div className="">
+    return <div className="landing-card-grid col">
         {renderDecks()}
     </div>
 }
+
+export default DeckCardContainer
