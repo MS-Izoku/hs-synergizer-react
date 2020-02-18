882360683af1508e78e0709a8e1f9b99b3bcfa7b
Martin Spicuzza <spicuzza157@gmail.com>
2020-02-18 13:29:51 -0600

    added one of two panels to the landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import DeckCardContainer from "../containers/DeckCardContainer";
 import LandingSplash from "../components/LandingSplash";
 import ArticleCard from "../components/ArticleCard";
+import { Link } from "react-router-dom";
 
 export default class LandingPage extends Component {
   constructor() {
@@ -15,18 +16,23 @@ export default class LandingPage extends Component {
         { name: "Deck4", upvotes: 10, set_img: "#", dustCost: 25000 },
         { name: "Deck5", upvotes: 10, set_img: "#", dustCost: 25000 }
       ],
+      expansionGuide: {
+        img:
+          "https://i.pinimg.com/originals/c8/52/8f/c8528f5de027470ffb7b181d1449874b.jpg",
+        title: "The Witchwood"
+      },
 
       article: {
         title: "I am an article!",
-        img: "https://i.pinimg.com/originals/c8/52/8f/c8528f5de027470ffb7b181d1449874b.jpg",
-        //"https://lh3.googleusercontent.com/proxy/T-X4CIS3lmr6n2LtypK-lvFGQX2_uOa4qp58TyZZlQf_21_4CeyxOP42yk0kk0ztOtNLwI-VgmvJ3y5ujl8tHWYZ3Qe77acD6ZFet-NK2ynnx_yp1Vvkp2oaLJHDnWgBUVZq10haSA53_5j6tKF8VwSsMiaFkRQLo7I0-sXy_Q5jLEFhdK0",
-        //"https://assets.rockpapershotgun.com/images/2019/11/hearthstone-battlegrounds-a.jpg/RPSS/resize/760x-1/format/jpg/quality/90",
+        img:
+          "https://i.pinimg.com/originals/c8/52/8f/c8528f5de027470ffb7b181d1449874b.jpg",
         author: "Mike Tyson",
         created_at: "2020-02-04 21:38:12"
       }
     };
   }
   render() {
+    console.log(this.state.expansionGuide.img);
     return (
       <>
         <section className="page-section">
@@ -35,9 +41,22 @@ export default class LandingPage extends Component {
             decks={this.state.decks}
             containerName="Hot Decks"
           />
-          {/* <div id="forums-hot" className="bg-info bottom-border">
-            <h2>Hot off the Forums</h2>
-          </div> */}
+        </section>
+        <section id="guides" className="page-section">
+          <div
+            className="expansion-guide"
+            style={{ 
+              background: "red",
+              backgroundImage: `url(${this.state.expansionGuide.img})` }}
+          >
+            <footer>
+              <Link to={"/guides"}>
+                <h2>{this.state.expansionGuide.title}</h2>
+              </Link>
+            </footer>
+          </div>
+
+          <div>Forums</div>
         </section>
         <section id="articles">
           <header className="section-header-wrapper">
