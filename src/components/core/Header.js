d6ad1c8bef771c02c51db52cf987a99f40d64aba
Martin Spicuzza <spicuzza157@gmail.com>
2020-02-18 19:36:25 -0600

    landing page logo is now a link to the homepage, stil needs sime pretty art

--- src/components/core/Header.js
+++ src/components/core/Header.js
@@ -2,19 +2,16 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import SideNav from "./SideNav";
 import UserNav from "./UserNav";
+import { Link } from "react-router-dom";
+import { withRouter } from "react-router";
 
-const Header = (props) => {
-  const logo = "https://image.flaticon.com/icons/png/512/36/36285.png";
-
+const Header = props => {
   return (
     <Router>
       <div className="nav-header-wrapper">
         <header className="nav-header">
-          <SideNav handleNavToggle={props.handleNavToggle}/>
-
-          <div className="nav-header logo">
-            <img src={logo} id="site-logo" alt="logo"/>
-          </div>
+          <SideNav handleNavToggle={props.handleNavToggle} />
+          <PageLogo history={props.history}/>
           <UserNav />
         </header>
       </div>
@@ -22,4 +19,21 @@ const Header = (props) => {
   );
 };
 
-export default Header;
+const PageLogo = props => {
+  const logo = "https://image.flaticon.com/icons/png/512/36/36285.png";
+  return (
+    <Link
+      to="/"
+      onClick={(event) => {
+        event.preventDefault();
+        props.history.push("/");
+      }}
+    >
+      <div className="nav-header logo">
+        <img src={logo} id="site-logo" alt="logo" />
+      </div>
+    </Link>
+  );
+};
+
+export default withRouter(Header);
